1a76444e760b72211af9b568d94f175b3582c2ff
jbaldwin <jbaldwin@spotx.tv>
2021-07-10 14:19:57 -0600

    actix hello world shared app state

--- actix/src/main.rs
+++ actix/src/main.rs
@@ -1,27 +1,51 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+
+struct AppState {
+    app_name: &'static str,
+    counter: AtomicUsize,
+}
+
+impl AppState {
+    pub fn new(app_name: &'static str) -> Self {
+        Self {
+            app_name,
+            counter: AtomicUsize::new(1)
+        }
+    }
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World")
+async fn index<'a>(app_data: web::Data<AppState>) -> impl Responder {
+    let counter = app_data.counter.fetch_add(1, Ordering::Relaxed);
+    let data = format!("Hello {} you are the {} visitor!", app_data.app_name, counter);
+    return HttpResponse::Ok().body(data);
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+    return HttpResponse::Ok().body(req_body);
 }
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey There!")
+    return HttpResponse::Ok().body("Hey There!");
 }
+
+static APP_NAME: &'static str = "Actix";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_data = web::Data::new(AppState::new(APP_NAME));
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
+            .app_data(app_data.clone())
+            .service(index)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
+    .keep_alive(75)
     .bind("127.0.0.1:8080")?
     .run()
     .await
